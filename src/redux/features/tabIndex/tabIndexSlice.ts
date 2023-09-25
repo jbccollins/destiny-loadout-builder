3ffc867ca6fc9ae3374f01617823f11f6f31455b
James Collins <jbccollins@users.noreply.github.com>
2023-09-25 02:15:13 -0400

    Add tab url param (#232)
    * Add tab url param and recall
    
    * Fix the tab bug causing double analyzer web worker

--- src/redux/features/tabIndex/tabIndexSlice.ts
+++ src/redux/features/tabIndex/tabIndexSlice.ts
@@ -2,6 +2,7 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 import { AppState } from '@dlb/redux/store';
 
+import { ETabType } from '@dlb/types/Tab';
 import { NIL, v4 as uuid } from 'uuid';
 
 export interface TabIndexState {
@@ -21,6 +22,17 @@ export const tabIndexSlice = createSlice({
 		setTabIndex: (state, action: PayloadAction<number>) => {
 			state.value = action.payload;
 			state.uuid = uuid();
+			// Add the tab as a new query param to the URL
+			// We only want to support linking to the analyze tab
+			const url = new URL(window.location.href);
+			const params = new URLSearchParams(url.search);
+			if (action.payload !== ETabType.ANALYZE) {
+				params.delete('tab');
+			} else {
+				params.set('tab', action.payload.toString());
+			}
+			url.search = params.toString();
+			window.history.replaceState({}, '', url.toString());
 		},
 	},
 });
---
+++ src/types/Tab.ts
@@ -0,0 +1,13 @@
+export enum ETabType {
+	BUILD = 0,
+	ANALYZE = 1,
+	SETTINGS = 2,
+	ABOUT = 3,
+}
+
+export const TabTypeList = [
+	ETabType.BUILD,
+	ETabType.ANALYZE,
+	ETabType.SETTINGS,
+	ETabType.ABOUT,
+];

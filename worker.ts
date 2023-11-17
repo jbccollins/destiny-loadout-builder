ae2bf162a7914c30a4bff67ec9040b81427835fa
M7ilan <distny2004@gmail.com>
2023-11-17 04:46:24 +0300

    Remove unnecessary code

--- worker.ts
+++
@@ -1,9 +0,0 @@
-// This is a module worker, so we can use imports (in the browser too!)
-// import pi from "./utils/pi"
-import { noop } from 'lodash';
-
-addEventListener('message', (event: MessageEvent<number>) => {
-	postMessage('derp');
-});
-
-export default noop;

ccdbaf06e30c1665e684e21a7e163dd34649358a
M7ilan <distny2004@gmail.com>
2023-11-19 18:26:27 +0300

    Add middleware file

---
+++ middleware.ts
@@ -0,0 +1,7 @@
+// Middleware Docs - https://nextjs.org/docs/app/building-your-application/routing/middleware
+
+import { NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {
+	// console.log("middleware");
+}
